2aa12f756cff204150641c3fb03a70bbdc321b9b
yknagas <130370169+yknagas@users.noreply.github.com>
2023-05-27 18:39:07 +0900

    Create btn_slide.js

---
+++ js/btn_slide.js
@@ -0,0 +1,71 @@
+/*window.onload = function(){};*/
+
+    var images = ['url(../kaien_kihon_imitate/image/cat.jpg) ',
+    'url(../kaien_kihon_imitate/image/hato.jpg)', 
+    'url(../kaien_kihon_imitate/image/inko.jpg)', 
+    'url(../kaien_kihon_imitate/image/sheep.jpg', 
+    'url(../kaien_kihon_imitate/image/carp.jpg)'
+    ];
+
+    var target = document.getElementById('slide');
+    var sp_target = document.getElementById('sp_slide');
+    /*var right = document.getElementById('right');
+    var left = document.getElementById('left');
+    var right_btn = document.getElementById('right');
+    var left_btn = document.getElementById('left');*/
+
+
+    var count = 0;
+
+    function change() {
+        target.style.backgroundImage = images[count];
+        sp_target.style.backgroundImage = images[count];
+    }
+
+
+    function Next() {
+        if(count == images.length - 1){
+            count = 0;
+        } else {
+            count++;
+        }
+        change();
+    }
+
+    function Back() {
+        if(count == 0){
+            count = images.length - 1
+        } else {
+            count--
+        }
+        change();
+    }
+
+    /*
+    right_btn.addEventListener('click', Next);
+
+    left_btn.addEventListener   ('click', Back);
+
+
+
+    /*
+    function goNext(){
+        if(count == images.length - 1){
+            count = 0;
+        } else {
+            count++;
+        }
+        change();
+    }
+
+    function goBack(){
+        if(count == 0){
+            count = images.length - 1;
+        } else {
+            count--;
+        }
+        change();
+    }
+
+    right.addEventListener('click',goNext,false);
+    left.addEventListener('click',goBack,false);*/
